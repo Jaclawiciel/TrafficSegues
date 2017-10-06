2f69e2324e725fb82164945ef47c00879b655583
Jacek Gałka <jacgalka@icloud.com>
2017-10-06 14:51:06 +0200

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Jacek Gałka on 06.10.2017.
+//  Copyright © 2017 Jacek Gałka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
